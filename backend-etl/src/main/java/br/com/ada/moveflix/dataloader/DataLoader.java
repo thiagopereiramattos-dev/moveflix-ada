b73d36c9bf0ca31fe1d789025aa7ba2e5c2da70c
Thiago Mattos <thiagopereiramattos@gamil.com>
2025-09-29 22:59:07 -0300

    ajuste para teste pipeline

--- backend-etl/src/main/java/br/com/ada/moveflix/dataloader/DataLoader.java
+++ backend-etl/src/main/java/br/com/ada/moveflix/dataloader/DataLoader.java
@@ -69,7 +69,7 @@ public class DataLoader implements CommandLineRunner {
         logger.info("🛑 Finalizando aplicação...");
         logger.info("🔍 Profile ativo: {}", System.getProperty("spring.profiles.active"));
 
-        if (!"test".equals(System.getProperty("spring.profiles.active"))) {
+        if (!"test".equals(System.getProperty("spring.profiles.active")) && (!"ci".equals(System.getProperty("spring.profiles.active")))) {
             // Fecha o contexto do Spring e encerra a JVM com código de saída 0 (sucesso)
             int exitCode = SpringApplication.exit(context, () -> 0);
             System.exit(exitCode);
