7f57f071ed5ec648255ea20f677c1f7a63b800dd
Thiago Mattos <thiagopereiramattos@gamil.com>
2025-09-29 22:34:44 -0300

    dataloader

--- backend-etl/src/main/java/br/com/ada/moveflix/dataloader/DataLoader.java
+++ backend-etl/src/main/java/br/com/ada/moveflix/dataloader/DataLoader.java
@@ -7,11 +7,15 @@ import br.com.ada.moveflix.service.etl.FilmeETLService;
 import br.com.ada.moveflix.service.etl.GeneroETLService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
 
 @Component
 @Profile("!test")
@@ -39,8 +43,14 @@ public class DataLoader implements CommandLineRunner {
         this.context = context;
     }
 
+    @Autowired
+    private Environment environment;
+
     @Override
     public void run(String... args) throws Exception {
+
+        logger.info("🔍 Profiles ativos: {}", Arrays.toString(environment.getActiveProfiles()));
+
         generoService.importarCSV("csv/generos.csv");
         logger.info("✅ Importação de Genero finalizada com sucesso");
 
@@ -57,9 +67,13 @@ public class DataLoader implements CommandLineRunner {
         logger.info("✅ Views do datamart atualizadas com sucesso");
 
         logger.info("🛑 Finalizando aplicação...");
+        logger.info("🔍 Profile ativo: {}", System.getProperty("spring.profiles.active"));
 
-        // Fecha o contexto do Spring e encerra a JVM com código de saída 0 (sucesso)
-        int exitCode = SpringApplication.exit(context, () -> 0);
-        System.exit(exitCode);
+        if (!"test".equals(System.getProperty("spring.profiles.active"))) {
+            // Fecha o contexto do Spring e encerra a JVM com código de saída 0 (sucesso)
+            int exitCode = SpringApplication.exit(context, () -> 0);
+            System.exit(exitCode);
+            //SpringApplication.exit(context, () -> 0);
+        }
     }
 }
